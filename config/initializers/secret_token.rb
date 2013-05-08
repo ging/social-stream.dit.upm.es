9d8c0b13acc5dd9c5b302b10d8104798a47f5abb
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-08 16:45:06 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStreamDitUpmEs::Application.config.secret_token = '1e3551a6749b49647612e4b93466bceedbdb6643d5b916663b2ac194d51341288d4af805de80f9e23be497fb09e3b61539f13e10ededbc19cb590bcda4d962c5'
